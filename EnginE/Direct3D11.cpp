80069d7c4c6190f93a00c60e4f3c6d29726e54ad
sergsenta1995 <sergsenta1995@gmail.com>
2015-08-17 15:04:57 +0400

    Init + Create Direct3D11

---
+++ EnginE/Direct3D11.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Direct3D11.h"
+
+
+CDirect3D11::CDirect3D11()
+{
+}
+
+
+CDirect3D11::~CDirect3D11()
+{
+}
---
+++ EnginE/Direct3D11.h
@@ -0,0 +1,8 @@
+#pragma once
+class CDirect3D11
+{
+public:
+	CDirect3D11();
+	~CDirect3D11();
+};
+
